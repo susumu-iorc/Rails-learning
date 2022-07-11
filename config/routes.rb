3c07587dcced44264dc9498609d41d2e5d3805d9
susumu <susumu@iorc.net>
2022-07-11 18:16:18 +0900

    tutorial5 end.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'users/new'
   root 'static_pages#home'
   #get 'static_pages/help'
   get '/help', to:'static_pages#help'
@@ -6,5 +7,8 @@ Rails.application.routes.draw do
   get '/about', to:'static_pages#about'
   #get 'static_pages/contact'
   get '/contact', to:'static_pages#contact'
+
+  get '/signup', to:"users#new"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
 end
