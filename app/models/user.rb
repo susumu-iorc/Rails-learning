7d87b99c04e83258932f111d78bb29a407370303
susumu <susumu@iorc.net>
2022-07-11 20:24:29 +0900

    end t6

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-    before_save { self.email =emaild.downcase}
+    before_save { self.email = email.downcase}
     validates   :name,    presence: true,
                           length: {maximum: 50 }
     
@@ -9,6 +9,7 @@ class User < ApplicationRecord
                             format:{with: VALID_EMAIL_REGEX},
                             uniqueness: {case_sensitive: false}
 
-
+    has_secure_password
+    validates :password, presence: true, length: { minimum: 6 }
 
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -5,7 +5,7 @@ class UserTest < ActiveSupport::TestCase
   #   assert true
   # end
   def setup
-    @user = User.new(name:"Example User", email: "user@ecample.com")
+    @user = User.new(name:"Example User", email: "user@ecample.com", password: "foobar", password_confirmation: "foobar")
   end
   test "should be valid" do
     assert @user.valid?
@@ -50,5 +50,15 @@ class UserTest < ActiveSupport::TestCase
     duplicate_user.email = @user.email.upcase
     @user.save
     assert_not duplicate_user.valid?
+  end
+
+  test "password should be present (nnblank)" do
+    @user.password = @user.password_confirmation = " " * 6
+    assert_not @user.valid?
+  end
+
+  test "password should have a minimum length" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
   end
 end
