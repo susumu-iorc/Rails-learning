1b0cb7a103fb856c30d09aca2b3f59f1cb43dba2
susumu <susumu@iorc.net>
2022-07-12 02:12:25 +0900

    t10 end

--- Gemfile
+++ Gemfile
@@ -57,4 +57,7 @@ gem 'rails-controller-testing'
 
 gem 'bcrypt',         '3.1.13'
 
+gem 'faker',                   '2.20.0'
 gem 'bootstrap-sass', '3.4.1'
+gem 'will_paginate',           '3.3.1'
+gem 'bootstrap-will_paginate', '1.0.0'
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -26,6 +26,10 @@ module SessionsHelper
         end
     end
 
+  # 渡されたユーザーがカレントユーザーであればtrueを返す
+  def current_user?(user)
+    user && user == current_user
+  end
 
   
   # ユーザーがログインしていればtrue、その他ならfalseを返す
@@ -47,4 +51,14 @@ module SessionsHelper
     @current_user = nil
   end
   
+  # 記憶したURL（もしくはデフォルト値）にリダイレクト
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  # アクセスしようとしたURLを覚えておく
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
+  end
 end
