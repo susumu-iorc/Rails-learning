0011df42e253326be7dbc049bf2f2cb0342c2e2f
susumu <susumu@iorc.net>
2022-07-12 19:22:24 +0900

    13.2.2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,9 +11,12 @@ class UsersController < ApplicationController
   def index
     @users = User.paginate(page: params[:page])
   end
+
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
+  
   def new
     @user = User.new
   end
